c6d00ca1c4cef5f52c5be265e9650be67c835453
Vasyl Kovalov <raid3r81@gmail.com>
2023-08-20 15:48:59 +0300

    Create user and message migrations

---
+++ Server/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models;
+
+public class Message
+{
+	public int Id { get; set; }
+	[ForeignKey("FromUserId")]
+	public virtual User From { get; set; }
+	[ForeignKey("ToUserId")]
+	public virtual User To { get; set; }
+	public DateTime CreatedAt { get; set; }
+	[StringLength(5000)]
+	public string Text { get; set; }
+}

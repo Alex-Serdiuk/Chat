830fe81331e40983643055e23bfb876f269df452
Vasyl Kovalov <raid3r81@gmail.com>
2023-08-20 15:48:59 +0300

    Create user and message migrations

---
+++ Server/Models/ServerContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models;
+
+public class ServerContext : DbContext
+{
+	public ServerContext(): base() { }
+	public ServerContext(DbContextOptions options) : base(options) { }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder.UseSqlServer("Data Source=SILVERSTONE\\SQLEXPRESS;Initial Catalog=Chat;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=60;Encrypt=False;Trust Server Certificate=False");
+
+	public DbSet<User> Users { get; set; } = null!;
+	public DbSet<Message> Messages { get; set; } = null!;
+}

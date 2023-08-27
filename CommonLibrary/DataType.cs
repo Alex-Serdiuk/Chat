4d72b2f46b94552f2290f51944188a428d592f6f
Vasyl Kovalov <raid3r81@gmail.com>
2023-08-27 16:28:59 +0300

    Begin with get messages

--- CommonLibrary/DataType.cs
+++ CommonLibrary/DataType.cs
@@ -6,4 +6,6 @@ public static class DataType
 {
 	public static readonly string Login = "LOGIN";
 	public static readonly string GetUsers = "GET_USERS";
+	
+	public static readonly string GetMessages = "GET_MESSAGES";
 }

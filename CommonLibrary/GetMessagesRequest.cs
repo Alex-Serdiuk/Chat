e89cf278df150934e095db1fe4a28fd5b340df8c
Vasyl Kovalov <raid3r81@gmail.com>
2023-08-27 16:28:59 +0300

    Begin with get messages

---
+++ CommonLibrary/GetMessagesRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonLibrary;
+
+public class GetMessagesRequest
+{
+	public ChatUser? From { get; set; }
+	public ChatUser? To { get; set; }
+	public int? AfterId { get; set; }
+}
